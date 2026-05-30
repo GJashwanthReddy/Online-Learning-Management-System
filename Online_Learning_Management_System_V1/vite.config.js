3090e888e35cbb23f53a2e26a02b9f2a7d293183
G Jashwanth Reddy <gangidijashwanthreddy@gmail.com>
2026-05-30 12:05:20 +0530

    Fix GitHub Pages deployment

--- Online_Learning_Management_System_V1/vite.config.js
+++ Online_Learning_Management_System_V1/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Online-Learning-Management-System/',
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+  },
 })
